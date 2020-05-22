05c8130a7ad89338d068fb9128aa395bb845de44
danelaisemberg <danel@ingenima.com>
2020-05-22 09:35:56 -0300

    1.2.3 Arreglos menores

--- page-templates/contacto.php
+++ page-templates/contacto.php
@@ -17,13 +17,26 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="bl-contacto" style="background-color:<?php the_field( 'bg_contacto' ); ?>">
 	<main id="main" class="site-main">
 		<div class="container">
-	    	<div class="row">
-	        	<div class="offset-md-2 col-md-6">
-	        		<h1 class="bl-title"><?php the_title(); ?></h1>
-	        		<?php the_content(); ?>
+	    	<div class="row <?php paddingY(); ?>">
+	        	<div class="col-md-5 d-flex align-items-center bl-message">
+	        		<div class="dummy-container text-white">
+	        			<h1 class="bl-title text-white"><?php the_field( 'titulo_contacto' ); ?></h1>
+	        			<?php the_field( 'texto_contacto' ); ?>
+	        			<div class="text-md-center pb-5">
+	        				<?php if ( the_field( 'img_contacto' ) ) { ?>
+								<img src="<?php the_field( 'img_contacto' ); ?>" alt="">
+		        			<?php } else { ?>
+								<img src="<?php echo site_url( ); ?>/wp-content/themes/blacklight/img/contact.svg" alt="">
+		        			<?php } ?>
+	        			</div>
+	        		</div>
+	        	</div> <!-- .col-md-6 -->
+	        	<div class="offset-md-1 col-md-5 px-5 py-3 bg-white rounded bl-contacto-form bl-shadow-2">
+	        		<h4 class="bl-title"><?php the_field( 'titulo_form' ); ?></h4>
+	        		<?php the_field( 'formulario_contacto' ); ?>
 	        	</div> <!-- .col-md-6 -->
 	    	</div> <!-- .row -->
     	</div> <!-- .container -->
